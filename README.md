7509dae96cd0126fd00e62deca7447eadafcb689
Nelson Kenmochi <nelsonkenmochi@gmail.com>
2020-10-05 12:18:54 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+<h3 align="center">
+    <img alt="Logo" title="#logo" width="300px" src="https://www.iconfinder.com/data/icons/weather-flat-14/64/weather02-512.png">
+</h3>
+<h1 align="center">
+    Weather App
+</h1>
+
+Projeto desenvolvimento para o desafio interno do grupo Code's Moon Technologies sobre o clima. Use o projeto como quiser, fique à vontade :)
+
+>*A API Key da Open Weather Map está pública neste repositório, peço que não utilize-a para ambiente de produção, mas, seja livre para utilizá-la ao testar meu sistema ou para desenvolver o seu próprio*
+
+## Desafio
+
+Um programa sobre o clima em que o usuário receba as condições do clima (temperatura atual, umidade e intensidade dos ventos, etc) e se é dia ou noite. O programa deve funcionar através de uma API.
+
+### Objetivos Primários:
+
+- [x] O usuário terá a opção de verificar o clima de 5 cidades pré-definidas (Manaus, Recife, São Paulo, Florianópolis e Goiânia). =
+
+- [x] Ao selecionar uma das cidades o usuário poderá visualizar o clima (temperatura atual, umidade e intensidade dos ventos) e se é dia ou noite.
+
+### Objetivos Secundários:
+
+- [ ] O usuário apenas ao digitar a opção/nome de uma cidade poderá verificar o clima da mesma através de um sistema condicional.
+
+## Stack utilizada
+
+- VSCode com: Live Server, Live SASS Compiler
+- HTML, SCSS, JavaScript
+- API Weather - https://openweathermap.org/
+
+## 📃 License
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE) para mais detalhes.
+
+---
+
+<h4 align="center">
+    Feito com ❤️ by <b>Nelson Kenmochi</b>
+</h4>
---
+++ index.js
@@ -0,0 +1,61 @@
+// I know it's insecure to put that here, but this key should be public
+const API_KEY = 'bfcae88cf13f3f2c05b4b4ebd3835341';
+
+function SendCityToSearch() {
+    const mainContainer = document.getElementById('main');
+    const cityToSearchInput = document.getElementById('myInput');
+    const cityToSearch = cityToSearchInput.value;
+    cityToSearchInput.value = ''
+    const result = document.getElementById('result');
+
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityToSearch}&appid=${API_KEY}&lang=pt_br`)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                result.innerHTML = `<h2>Não foi possível encontrar esta cidade/estado 😞</h2>`;
+                throw new Error(`Ocorreu um erro`)
+            }
+        })
+        .then(data => {
+            const currentTime = data.dt;
+            const isDay = (currentTime >= data.sys.sunrise && currentTime < data.sys.sunset);
+
+            if (isDay) {
+                mainContainer.classList.remove('dark');
+                mainContainer.classList.add('light');
+            } else {
+                mainContainer.classList.add('dark');
+                mainContainer.classList.remove('light');
+            }
+
+            const temperature = (data.main.temp - 273.15).toFixed(1);
+
+            result.innerHTML = `
+            <h2>Em ${cityToSearch} está de ${isDay ? 'dia! 🌞' : 'noite! 🌃'} </h2>
+
+            <!-- Information -->
+            <p>Sigla País: ${data.sys.country}</p>
+            <p>Temperatura: ${temperature} °C  ${ temperature > 30 ? '🥵' : (temperature < 25 ? '🥶' : '😃') }</p>
+            <p>Clima atual: ${data.weather[0].description}
+                <img style="vertical-align: middle" src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" alt="${data.weather[0].description}"/>
+            </p>
+            <p>Umidade: ${data.main.humidity} %</p>
+            <p>Velocidade dos ventos: ${data.wind.speed} m/s</p>
+            
+            <!-- Google Maps -->
+            <div class="map-link">
+                <a target='_blank' href='https://www.google.com/maps/search/@${data.coord.lat},${data.coord.lon},15.25z'>Abrir em Google Maps</a>
+            <div>`;
+            console.log(data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+function SearchByCity(cityName) {
+    const cityToSearchInput = document.getElementById('myInput');
+    cityToSearchInput.value = cityName;
+    SendCityToSearch();
+}
